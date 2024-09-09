fc79f3d496f6b85304df40501c4a9c5584a98f21
Dmytro Slipchenko <dslipchenko@gmail.com>
2024-09-10 00:37:50 +0300

    feat: add desk case section

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
